2a46570ba22f065aa672b6667f6ae7256eb838eb
Coder6000 <antonio.rudacevschi@outlook.de>
2024-11-20 20:32:06 +0100

    added fundamental functions

--- tictactoe.c
+++ tictactoe.c
@@ -2,14 +2,27 @@
 
 void printBoard();
 void checkWinner();
+void playerOneTurn();
+void playerTwoTurn();
+void game();
+
+int grid[4][4];
+
+int isPlayerOneTurn = 1;
+int isPlayerTwoTurn = 0;
+
+int userInput;
 
 int main(void){
     printBoard();
+
+    game();
+    playerOneTurn();
     return 0;
 }
 
 void printBoard(void){
-    int grid[4][4];
+    
 
     printf("|-----------|\n");
 
@@ -24,41 +37,82 @@ void printBoard(void){
         }
         printf("\n|-----------|\n");
     }
+    printf("\n");
+}
+
+void game(){
+
+    while(1 < 0){
+        if(isPlayerOneTurn == 1 && isPlayerTwoTurn == 0){
+            playerOneTurn();
+            isPlayerOneTurn = 0;
+            isPlayerTwoTurn = 1;
+        }
+
+        if (isPlayerOneTurn == 0 && isPlayerTwoTurn == 1){
+            playerTwoTurn();
+            isPlayerTwoTurn = 0;
+            isPlayerOneTurn = 1;
+        }
+
+    }
+    
+}
+
+void playerOneTurn(){
+    printf("Which field would you like to take Player One?: ");
+    scanf("%d", &userInput);
+
+    switch(userInput){
+        case 0:
+            grid[0][0] = 'X';
+        break;
+        
+        case 1:
+            grid[0][0] = 'X';
+        break;
+    }
+}
+
+void playerTwoTurn(){
+    printf("Which field would you like to take Player Two?: ");
+    scanf("%d", &userInput);
 }
 
 void checkWinner(){
-    int winCondition[3][3];
+    char winCondition[3][3];
 
     //PLAYER X
 
     //ROWS
-    if(winCondition[0][0] == "X" && winCondition[0][1] == "X" && winCondition[0][2] == "X"){}
-    if(winCondition[1][0] == "X" && winCondition[1][1] == "X" && winCondition[1][2] == "X"){}
-    if(winCondition[2][0] == "X" && winCondition[2][1] == "X" && winCondition[2][2] == "X"){}
+    if(winCondition[0][0] == 'X' && winCondition[0][1] == 'X' && winCondition[0][2] == 'X') {}
+    if(winCondition[1][0] == 'X' && winCondition[1][1] == 'X' && winCondition[1][2] == 'X') {}
+    if(winCondition[2][0] == 'X' && winCondition[2][1] == 'X' && winCondition[2][2] == 'X') {}
 
     //COLUMNS
-    if(winCondition[0][0] == "X" && winCondition[1][0] == "X" && winCondition[2][0] == "X"){}
-    if(winCondition[0][1] == "X" && winCondition[1][1] == "X" && winCondition[2][1] == "X"){}
-    if(winCondition[0][2] == "X" && winCondition[1][2] == "X" && winCondition[2][2] == "X"){}
+    if(winCondition[0][0] == 'X' && winCondition[1][0] == 'X' && winCondition[2][0] == 'X') {}
+    if(winCondition[0][1] == 'X' && winCondition[1][1] == 'X' && winCondition[2][1] == 'X') {}
+    if(winCondition[0][2] == 'X' && winCondition[1][2] == 'X' && winCondition[2][2] == 'X') {}
 
     //DIAGONALS
-    if(winCondition[0][0] == "X" && winCondition[1][1] == "X" && winCondition[2][2] == "X"){}
-    if(winCondition[0][2] == "X" && winCondition[1][1] == "X" && winCondition[2][0] == "X"){}
+    if(winCondition[0][0] == 'X' && winCondition[1][1] == 'X' && winCondition[2][2] == 'X') {}
+    if(winCondition[0][2] == 'X' && winCondition[1][1] == 'X' && winCondition[2][0] == 'X') {}
 
     //PLAYER Y
 
     //ROWS
-    if(winCondition[0][0] == "O" && winCondition[0][1] == "O" && winCondition[0][2] == "O"){}
-    if(winCondition[1][0] == "O" && winCondition[1][1] == "O" && winCondition[1][2] == "O"){}
-    if(winCondition[2][0] == "O" && winCondition[2][1] == "O" && winCondition[2][2] == "O"){}
+    if(winCondition[0][0] == 'O' && winCondition[0][1] == 'O' && winCondition[0][2] == 'O') {}
+    if(winCondition[1][0] == 'O' && winCondition[1][1] == 'O' && winCondition[1][2] == 'O') {}
+    if(winCondition[2][0] == 'O' && winCondition[2][1] == 'O' && winCondition[2][2] == 'O') {}
 
     //COLUMNS
-    if(winCondition[0][0] == "O" && winCondition[1][0] == "O" && winCondition[2][0] == "O"){}
-    if(winCondition[0][1] == "O" && winCondition[1][1] == "O" && winCondition[2][1] == "O"){}
-    if(winCondition[0][2] == "O" && winCondition[1][2] == "O" && winCondition[2][2] == "O"){}
+    if(winCondition[0][0] == 'O' && winCondition[1][0] == 'O' && winCondition[2][0] == 'O') {}
+    if(winCondition[0][1] == 'O' && winCondition[1][1] == 'O' && winCondition[2][1] == 'O') {}
+    if(winCondition[0][2] == 'O' && winCondition[1][2] == 'O' && winCondition[2][2] == 'O') {}
 
     //DIAGONALS
-    if(winCondition[0][0] == "O" && winCondition[1][1] == "O" && winCondition[2][2] == "O"){}
-    if(winCondition[0][2] == "O" && winCondition[1][1] == "O" && winCondition[2][0] == "O"){}
+    if(winCondition[0][0] == 'O' && winCondition[1][1] == 'O' && winCondition[2][2] == 'O') {}
+    if(winCondition[0][2] == 'O' && winCondition[1][1] == 'O' && winCondition[2][0] == 'O') {}
 
 }
+
